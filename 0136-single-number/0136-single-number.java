af1c33fe1bd3695b3ba2b3d21ccb1146b503cb54
Vanisha Tiwari <vanishatiwari831@gmail.com>
2026-07-11 20:08:44 +0530

    Time: 1 ms (99.97%), Space: 46.8 MB (82.08%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xorr=0;
+        for(int num:nums){
+            xorr^=num;
+        }
+        return xorr;
+        
+    }
+}
